c81d373968e5062a11f5851ca33424d9e17fd003
prasad <ashoksurisetty0508@gmail.com>
2023-04-17 12:23:40 +0530

    create frontend script & config files

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,6 @@
-cd mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
-
 systemctl enable mongod
 systemctl start mongod
 
-#Edit the file and replace 127.0.0.1 with 0.0.0.0
+#Edit and Replace file 127.0.0.1 with 0.0.0.0
