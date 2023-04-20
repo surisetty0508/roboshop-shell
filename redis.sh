dec94c33d6b88534c06a143f3b334d77e5aa9e65
prasad <ashoksurisetty0508@gmail.com>
2023-04-20 11:05:43 +0530

    adding print statements

--- redis.sh
+++ redis.sh
@@ -5,7 +5,7 @@ echo -e '\e[36m>>>>>>>>install redis <<<<<<<<\e[0m'
 dnf module enable redis:remi-6.2 -y
 yum install redis -y
 
-echo -e '\e[36m>>>>>>>>update redis listen address <<<<<<<<\e[0m
+echo -e '\e[36m>>>>>>>>update redis listen address <<<<<<<<\e[0m'
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
 
 
