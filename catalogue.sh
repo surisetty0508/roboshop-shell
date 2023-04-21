4d52a70ae23bf6987d4700262afad5b7368afd5f
prasad <ashoksurisetty0508@gmail.com>
2023-04-21 22:06:24 +0530

    adding print statements

--- catalogue.sh
+++ catalogue.sh
@@ -39,4 +39,4 @@ echo -e '\e[36m>>>>>>>>install mongodb client <<<<<<<<\e[0m'
 yum install mongodb-org-shell -y
 
 echo -e '\e[36m>>>>>>>>load schema<<<<<<<<\e[0m'
-mongo --host mongodb.dev.devops72.online </app/schema/catalogue.js
+mongo --host mongodb-dev.devops72.online </app/schema/catalogue.js
